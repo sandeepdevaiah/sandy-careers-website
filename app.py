82f1ae4f236074ad6644bc2c64448b7fc2301a80
SD_redDevil <sandeep.devaiah.b@gmail.com>
2024-12-25 12:44:10 +0000

    add the flask project

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("hello Sandy")
+from flask import Flask
+
+app = Flask(__name__) #creating an object of the class Flask
+
+
+@app.route("/")
+def hello_world():
+  return "Hello, Man!! "
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
